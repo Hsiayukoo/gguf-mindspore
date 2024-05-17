def14c44954e228676cd53754442ba842583350b
hsiayukoo_mindspore <81662220+Hsiayukoo@users.noreply.github.com>
2024-05-17 14:40:56 +0800

    initial
    initial : can read information from gguf format file.

---
+++ constant.py
@@ -0,0 +1,225 @@
+from enum import Enum
+import abc
+from typing import Any, List, TypeVar
+import numpy as np
+
+
+ALIGNMENT = np.uint32(32)
+
+
+class GGUFException(Exception):
+    pass
+
+
+class GGMLType(Enum):
+    F32 = np.uint32(0)
+    F16 = np.uint32(1)
+    Q4_0 = np.uint32(2)
+    Q4_1 = np.uint32(3)
+    Q5_0 = np.uint32(6)
+    Q5_1 = np.uint32(7)
+    Q8_0 = np.uint32(8)
+    Q8_1 = np.uint32(9)
+    Q2_K = np.uint32(10)
+    Q3_K = np.uint32(11)
+    Q4_K = np.uint32(12)
+    Q5_K = np.uint32(13)
+    Q6_K = np.uint32(14)
+    Q8_K = np.uint32(15)
+    IQ2_XXS = np.uint32(16)
+    IQ2_XS = np.uint32(17)
+    IQ3_XXS = np.uint32(18)
+    IQ1_S = np.uint32(19)
+    IQ4_NL = np.uint32(20)
+    IQ3_S = np.uint32(21)
+    IQ2_S = np.uint32(22)
+    IQ4_XS = np.uint32(23)
+    I8 = np.uint32(24)
+    I16 = np.uint32(25)
+    I32 = np.uint32(26)
+    I64 = np.uint32(27)
+    F64 = np.uint32(28)
+    IQ1_M = np.uint32(29)
+
+
+class GGUFMetadataValueType(Enum):
+    UINT8 = np.uint32(0)
+    INT8 = np.uint32(1)
+    UINT16 = np.uint32(2)
+    INT16 = np.uint32(3)
+    UINT32 = np.uint32(4)
+    INT32 = np.uint32(5)
+    FLOAT32 = np.uint32(6)
+    BOOL = np.uint32(7)
+    STRING = np.uint32(8)
+    ARRAY = np.uint32(9)
+    UINT64 = np.uint32(10)
+    INT64 = np.uint32(11)
+    FLOAT64 = np.uint32(12)
+
+
+# NOTE: we set bool in the set.
+GGUF_METADATA_TYPR_NUMBER_SET = {GGUFMetadataValueType.UINT8, GGUFMetadataValueType.INT8,
+                                 GGUFMetadataValueType.UINT16, GGUFMetadataValueType.INT16,
+                                 GGUFMetadataValueType.UINT32, GGUFMetadataValueType.INT32,
+                                 GGUFMetadataValueType.FLOAT32, GGUFMetadataValueType.UINT64,
+                                 GGUFMetadataValueType.INT64, GGUFMetadataValueType.FLOAT64,
+                                 GGUFMetadataValueType.BOOL}
+
+
+class FormatCharacter:
+    UINT8 = "B"
+    INT8 = "b"
+    UINT16 = "H"
+    INT16 = "h"
+    UINT32 = "I"
+    INT32 = "i"
+    UINT64 = "Q"
+    INT64 = "q"
+    FLOAT32 = "f"
+    FLOAT64 = "d"
+    BOOL = "?"
+
+
+FORMAT_CHARACTER_DICT = {
+    "UINT8": "B",
+    "INT8": "b",
+    "UINT16": "H",
+    "INT16": "h",
+    "UINT32": "I",
+    "INT32": "i",
+    "UINT64": "Q",
+    "INT64": "q",
+    "FLOAT32": "f",
+    "FLOAT64": "d",
+    "BOOL": "?"
+}
+
+FORMAT_NP_TYPE_DICT = {
+    "B": np.uint8,
+    "b": np.int8,
+    "H": np.uint16,
+    "h": np.int16,
+    "I": np.uint32,
+    "i": np.int32,
+    "Q": np.uint64,
+    "q": np.int64,
+    "f": np.float32,
+    "d": np.float64,
+    "?": np.bool_
+}
+
+
+class GGUFString:
+    def __init__(self, length: np.uint64, string: str):
+        """
+        :param length: The length of the string, in bytes. -- UINT64
+        :param string: The string as a UTF-8 non-null-terminated string.
+        """
+        self.length = length
+        self.string = string
+
+    def __str__(self):
+        return self.string
+
+
+T = TypeVar("T", np.uint8, np.int8, np.uint16, np.int16, np.uint32, np.int32, np.float32, np.uint64, np.int64,
+            np.float64, np.bool_, GGUFString)
+
+K = TypeVar("K", np.uint8, np.int8, np.uint16, np.int16, np.uint32, np.int32, np.float32, np.uint64, np.int64,
+            np.float64)
+
+
+class GGUFMetadataValue(metaclass=abc.ABCMeta):
+    def __init__(self, value: Any):
+        self.value = value
+
+    def __str__(self):
+        return "{0}".format(self.value)
+
+
+class GGUFMetadataValueT(GGUFMetadataValue):
+    def __init__(self, value: T):
+        self.value = value
+
+
+class GGUFMetadataValueGGUFArray(GGUFMetadataValue):
+    def __init__(self, type_: GGUFMetadataValueType, length: np.uint64, value: List[GGUFMetadataValue]):
+        """
+        :param type_: Any value type is valid, including arrays.
+        :param length: UINT64.
+        :param value: The array of values.
+        """
+        self.type = type_
+        self.length = length
+        self.value = value
+
+
+class GGUFMetadataKV:
+    def __init__(self, key: GGUFString, value_type: GGUFMetadataValueType, value: GGUFMetadataValue):
+        self.key = key
+        self.value_type = value_type
+        self.value = value
+
+    def __str__(self):
+        return "key: {}, value_type: {}, value: {}".format(str(self.key), str(self.value_type), str(self.value))
+
+
+class GGUFHeader:
+    def __init__(self, magic: np.uint32, version: np.uint32, tensor_count: np.uint64, metadata_kv_count: np.uint64,
+                 metadata_kv: List[GGUFMetadataKV]):
+        """
+        :param magic: UINT32.
+        :param version: UINT32.
+        :param tensor_count: UINT64.
+        :param metadata_kv_count: UINT64.
+        :param metadata_kv: List[GGUFMetadataKV]
+        """
+        self.magic = magic
+        self.version = version
+        self.tensor_count = tensor_count
+        self.meta_kv_count = metadata_kv_count
+        self.metadata_kv = metadata_kv
+
+
+def align_offset(offset: np.uint64) -> np.uint64:
+    """
+    :param offset:  UINT64.
+    :return: UINT64.
+    """
+    return (offset + (ALIGNMENT - (offset % ALIGNMENT)) % ALIGNMENT).astype(np.uint64)
+
+
+class GGUFTensorInfo:
+    def __init__(self, name: GGUFString, n_dimensions: np.uint32, dimensions: [np.uint64], type_: GGMLType, offset: np.uint64):
+        """
+        :param name:
+        :param n_dimensions: UINT32
+        :param dimensions: UINT64
+        :param type_:
+        :param offset: UINT64
+        """
+        self.name = name
+        self.n_dimensions = n_dimensions
+        self.dimensions = dimensions
+        self.type = type_
+        self.offset = offset
+
+    def __str__(self):
+        return "name: {0}, n_dimensions: {1}, dimensions: {2}, type: {3}, offset: {4}".format(str(self.name),
+               self.n_dimensions, self.dimensions, str(self.type), self.offset)
+
+
+class GGUFFile:
+    def __init__(self, header: GGUFHeader, tensor_infos: List[GGUFTensorInfo], padding: List[np.uint8],
+                 tensor_data: List[np.uint8]):
+        """
+        :param header:
+        :param tensor_infos:
+        :param padding: UINT8
+        :param tensor_data: UINT8
+        """
+        self.header = header
+        self.tensor_infos = tensor_infos
+        self.padding = padding
+        self.tensor_data = tensor_data
---
+++ read_gguf.py
@@ -0,0 +1,161 @@
+import struct
+from typing import List, BinaryIO
+
+import numpy as np
+
+from constant import FormatCharacter, GGUFException, GGUFTensorInfo, GGUFMetadataKV, GGUFString, \
+    GGUFMetadataValueType, GGUF_METADATA_TYPR_NUMBER_SET, FORMAT_CHARACTER_DICT, FORMAT_NP_TYPE_DICT, K, GGMLType
+
+VALID_MAGIC_NUMBER = b"GGUF"
+VALID_GGUF_VERSION = 3
+ENCODING = "utf-8"
+
+
+class GGUFLoader:
+    def __init__(self, gguf_file_path: str):
+        """
+        :param gguf_file_path: gguf_file_path
+        """
+        self.gguf_file_path: str = gguf_file_path
+        self.tensor_count: np.uint64 = np.uint64(0)
+        self.metadata_kv_count: np.uint64 = np.uint(64)
+        self.metadata: List[GGUFMetadataKV] = []
+        self.tensor_infos: List[GGUFTensorInfo] = []
+        self.f: BinaryIO = None
+
+    @staticmethod
+    def auto_struct_unpack(f: BinaryIO, format_: str) -> K:
+        standard_size = struct.calcsize(format_)
+        value = FORMAT_NP_TYPE_DICT[format_](struct.unpack(format_, f.read(standard_size))[0])
+        return value
+
+    @staticmethod
+    def get_gguf_string(f: BinaryIO) -> GGUFString:
+        length = GGUFLoader.auto_struct_unpack(f, FormatCharacter.UINT64)
+        string = f.read(length).decode(ENCODING)
+        return GGUFString(length=length, string=string)
+
+    @staticmethod
+    def get_gguf_metadata_value_type(f: BinaryIO) -> GGUFMetadataValueType:
+        temp_value_type = GGUFLoader.auto_struct_unpack(f, FormatCharacter.UINT32)
+        return GGUFMetadataValueType(temp_value_type)
+
+    @staticmethod
+    def get_gguf_metadata_value(f: BinaryIO, value_type: GGUFMetadataValueType):
+        if value_type in GGUF_METADATA_TYPR_NUMBER_SET:
+            return GGUFLoader.auto_struct_unpack(f, FORMAT_CHARACTER_DICT[value_type.name])
+        if value_type == GGUFMetadataValueType.STRING:
+            return f.read(struct.unpack(FormatCharacter.UINT64, f.read(8))[0]).decode(ENCODING)
+        if value_type == GGUFMetadataValueType.ARRAY:
+            array_value_type = GGUFMetadataValueType(GGUFLoader.auto_struct_unpack(f, FormatCharacter.UINT32))
+            array_length = GGUFLoader.auto_struct_unpack(f, FormatCharacter.UINT64)
+            return [GGUFLoader.get_gguf_metadata_value(f, array_value_type) for _ in range(array_length)]
+        raise GGUFException("unexpected metadata value type.")
+
+    def _set_up(self):
+        """
+        setUp: open file
+        :return:
+        """
+        self.f = open(self.gguf_file_path, "rb")
+
+    def _tear_down(self):
+        """
+        tearDown: close file
+        :return:
+        """
+        self.f.close()
+
+    def _check_magic_number(self):
+        """
+        check whether the magic number is right.
+        :return: void
+        """
+        if self.f.read(4) != VALID_MAGIC_NUMBER:
+            raise GGUFException("Invalid magic number")
+
+    def _check_version(self):
+        """
+        check whether the version is 3.
+        :return: void
+        """
+        if GGUFLoader.auto_struct_unpack(self.f, FormatCharacter.UINT32) != VALID_GGUF_VERSION:
+            raise GGUFException("Invalid version")
+
+    def _read_tensor_count(self):
+        """
+        read tensor numbers
+        :return: void
+        """
+        self.tensor_count = GGUFLoader.auto_struct_unpack(self.f, FormatCharacter.UINT64)
+
+    def _read_metadata_kv_count(self):
+        """
+        read meta data key-value numbers
+        :return: void
+        """
+        self.metadata_kv_count = GGUFLoader.auto_struct_unpack(self.f, FormatCharacter.UINT64)
+
+    def _read_metadata_key_value_pairs(self):
+        """
+        read metadata value.
+        :return:
+        """
+        for i in range(self.metadata_kv_count):
+            key = GGUFLoader.get_gguf_string(self.f)
+            value_type = GGUFLoader.get_gguf_metadata_value_type(self.f)
+            value = GGUFLoader.get_gguf_metadata_value(self.f, value_type)
+            metadata_kv = GGUFMetadataKV(key, value_type, value)
+            self.metadata.append(metadata_kv)
+
+    def _read_tensors_info(self):
+        """
+        get tensors info
+        :return:
+        """
+        for i in range(self.tensor_count):
+            name = GGUFLoader.get_gguf_string(self.f)
+            n_dimensions = GGUFLoader.auto_struct_unpack(self.f, FormatCharacter.UINT32)
+            dimensions = [GGUFLoader.auto_struct_unpack(self.f, FormatCharacter.UINT64) for _ in range(n_dimensions)]
+            type_ = GGMLType(GGUFLoader.auto_struct_unpack(self.f, FormatCharacter.UINT32))
+            offset = GGUFLoader.auto_struct_unpack(self.f, FormatCharacter.UINT64)
+            self.tensor_infos.append(GGUFTensorInfo(name, n_dimensions, dimensions, type_, offset))
+
+    def load_and_print(self):
+        """
+        main function to load GGUF info and print summary
+        :return: void
+        """
+        self._set_up()
+        try:
+            # CAUTION: these methods should execute by ORDER!
+            self._check_magic_number()
+            self._check_version()
+            self._read_tensor_count()
+            self._read_metadata_kv_count()
+            self._read_metadata_key_value_pairs()
+            self._read_tensors_info()
+        except GGUFException as e:
+            print(e)
+            self._tear_down()
+        finally:
+            self._tear_down()
+
+
+if __name__ == "__main__":
+    path = r"D:\PyGGUF\dummy.gguf"
+    gguf_loader = GGUFLoader(path)
+    gguf_loader.load_and_print()
+    metadata = gguf_loader.metadata
+    tensors_info = gguf_loader.tensor_infos
+    print("\n============ metadata ============")
+    for meta_data in metadata:
+        print(meta_data)
+    print("\n============ tensors info ============")
+    for tensor_info in tensors_info:
+        print(tensor_info)
+
+
+
+
+
